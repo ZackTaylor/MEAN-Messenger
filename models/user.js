3dcc1ab3e04432fe6dc2df3a8fad6c5f2b215173
Zack Tylor <taylor.zackory@gmail.com>
2017-09-27 13:33:20 -0500

    fix syntax errors in user model

--- models/user.js
+++ models/user.js
@@ -1,15 +1,15 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var mongooseUniqueValidator = requuire('mongoose-unique-validator');
+var mongooseUniqueValidator = require('mongoose-unique-validator');
 
 var schema = new Schema({
   firstName: {type: String, required: true},
   lastName: {type: String, required: true},
   password: {type: String, required: true},
   email: {type: String, required: true, unique: true},
-  messages: [{type: Shema.Types.ObjectId, ref: 'Message'}]
+  messages: [{type: Schema.Types.ObjectId, ref: 'Message'}]
 });
 
-shema.plugin(mongooseUniqueValidator);
+schema.plugin(mongooseUniqueValidator);
 
 module.exports = mongoose.model('User', schema);
