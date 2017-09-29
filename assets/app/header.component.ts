36a259b4afc90987c6fe61c0697c46a2c3a0ea51
Zack Tylor <taylor.zackory@gmail.com>
2017-09-28 20:24:42 -0500

    add proper activated styles to sow which tab is selected

--- assets/app/header.component.ts
+++ assets/app/header.component.ts
@@ -6,11 +6,11 @@ import { Component } from '@angular/core';
     <header class="row">
       <nav class="col-md-8 col-md-offset-2">
         <ul class="nav nav-pills">
-          <li>
-            <a >Messanger</a>
+          <li routerLinkActive="active">
+            <a [routerLink]="['/messages']">Messanger</a>
           </li>
-          <li>
-            <a >Authentication</a>
+          <li routerLinkActive="active">
+            <a [routerLink]="['/auth']">Authentication</a>
           </li>
         </ul>
       </nav>
