43f818722ea0c4c7a0b948d3c4109167e37dfc4a
Zack Tylor <taylor.zackory@gmail.com>
2017-09-28 19:28:43 -0500

    set up application to handle angular routing

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -4,15 +4,21 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from "./app.component";
 import { MessageComponent } from "./messages/message.component";
+import { MessagesComponent } from "./messages/messages.component";
 import { MessageListComponent } from "./messages/message-list.component";
 import { MessageInputComponent } from "./messages/message-input.component";
+import { AuthenticationComponent } from "./auth/authentication.component";
+import { HeaderComponent } from "./header.component";
 
 @NgModule({
     declarations: [
         AppComponent,
         MessageComponent,
+        MessagesComponent,
         MessageListComponent,
-        MessageInputComponent
+        MessageInputComponent,
+        AuthenticationComponent,
+        HeaderComponent
     ],
     imports: [BrowserModule, FormsModule],
     bootstrap: [AppComponent]
