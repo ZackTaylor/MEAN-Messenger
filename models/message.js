65d7fcdaef70238a6d4b84367ef50f1de05f8933
Zack Tylor <taylor.zackory@gmail.com>
2017-09-29 12:05:40 -0500

    add ability to save messages to the db

--- models/message.js
+++ models/message.js
@@ -3,7 +3,7 @@ var Schema = mongoose.Schema;
 
 var schema = new Schema({
   content: {type: String, required: true},
-  user: {type: Shema.Types.ObjectId, ref: 'User'}
+  user: {type: Schema.Types.ObjectId, ref: 'User'}
 });
 
 module.exports = mongoose.model('Message', schema);
