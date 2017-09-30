654dac7f0b8dba2a0e1744f822aea50cd3c6d5a5
Zack Tylor <taylor.zackory@gmail.com>
2017-09-30 13:39:02 -0500

    fix mispelled require

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var bcrypt = require('bcryptjs');
-var jwt = requre('jsonwebtoken');
+var jwt = require('jsonwebtoken');
 
 var User = require('../models/user');
 
