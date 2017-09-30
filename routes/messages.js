2eebb4fb618dd5ba890f70a69230274266d7abd3
Zack Tylor <taylor.zackory@gmail.com>
2017-09-30 18:29:38 -0500

    update messagge service to update messages with username and user id in the new message and update message routes

--- routes/messages.js
+++ routes/messages.js
@@ -7,6 +7,7 @@ var Message = require('../models/message');
 
 router.get('/', function(req, res, next) {
   Message.find()
+    .populate('user', 'firstName')
     .exec(function(err, messages) {
       if (err) {
         return res.status(500).json({
@@ -57,7 +58,8 @@ router.post('/', function (req, res, next) {
       user.save();
       res.status(201).json({
         message: 'Saved message.',
-        obj: result
+        obj: result,
+        username: user.firstName
       });
     });
   });
