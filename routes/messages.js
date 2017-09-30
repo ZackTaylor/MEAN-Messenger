c4515bbeb81b088746da31f2c96e24dca11fd90c
Zack Tylor <taylor.zackory@gmail.com>
2017-09-30 16:15:58 -0500

    setup backend to add current user as author of given message.

--- routes/messages.js
+++ routes/messages.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
+var jwt = require('jsonwebtoken');
 
+var User = require('../models/user');
 var Message = require('../models/message');
 
 router.get('/', function(req, res, next) {
@@ -19,9 +21,30 @@ router.get('/', function(req, res, next) {
     });
 });
 
+router.use('/', function(req, res, next) {
+  jwt.verify(req.query.token, 'secret', function(err, decoded) {
+    if (err) {
+      return res.status(401).json({
+        title: 'Not Authenticated',
+        error: err
+      });
+    }
+    next();
+  });
+});
+
 router.post('/', function (req, res, next) {
+  var decoded = jwt.decode(req.query.token);
+  User.findById(decoded.user._id, function functionName() {
+    if (err) {
+      return res.status(500).json({
+        title: 'An error occured.',
+        error: err
+      });
+    }
     var message = new Message({
-      content: req.body.content
+      content: req.body.content,
+      user: user
     });
     message.save(function(err, result) {
       if (err) {
@@ -30,11 +53,14 @@ router.post('/', function (req, res, next) {
           error: err
         });
       }
+      user.messages.push(result);
+      user.save();
       res.status(201).json({
         message: 'Saved message.',
         obj: result
       });
     });
+  });
 });
 
 router.patch('/:id', function (req, res, next) {
