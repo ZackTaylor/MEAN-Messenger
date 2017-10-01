79951d4cae72392c808d995ad0def6238500e9e7
Zack Tylor <taylor.zackory@gmail.com>
2017-10-01 11:47:10 -0500

    make ll catch methods in app emit the errors so that the message component can handle them and display the modal with the errors accordingly.

--- assets/app/errors/error.component.ts
+++ assets/app/errors/error.component.ts
@@ -1,14 +1,29 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { ErrorService } from "./error.service";
+
+import { Error } from "./error.model"
 
 @Component({
   selector: 'app-error',
   templateUrl: './error.component.html'
 })
-export class ErrorComponent {
+export class ErrorComponent implements OnInit{
   error: Error;
   display = 'none';
 
+  constructor(private ErrorService: ErrorService) {}
+
   onErrorHandled() {
     this.display = 'none';
+  }
+
+  ngOnInit(){
+    this.ErrorService.errorOccurred
+      .subscribe(
+        (error: Error) => {
+          this.error = error;
+          this.display = 'block';
+        }
+      );
   }
 }
